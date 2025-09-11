415f1a4e2434521b5a12daff1f5867b3fe7772b2
jonashelander <helanderjonas@gmail.com>
2025-09-11 15:07:24 +0200

    Optimized pictures, added favicon and meta description, added flexbox gap fix for safari | This should be the final commitgit add .! :)

--- js/script.js
+++ js/script.js
@@ -19,6 +19,57 @@ navBtnEL.addEventListener("click", function () {
   headerEL.classList.toggle("nav-open");
 })
 
+////////////////////////////////////////////////////////////
+//Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a")
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href")
+
+    //Scroll back to the top
+    if (href === "#") window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" })
+    }
+
+    //Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEL.classList.toggle("nav-open");
+    }
+  });
+});
+
+/////////////////////////////////////////////////////////
+//Sticky navigation
+const sectionHeroEL = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  console.log(ent);
+  if (!ent.isIntersecting) {
+    document.body.classList.add("sticky")
+  }
+  if (ent.isIntersecting) {
+    document.body.classList.remove("sticky")
+  }
+}, {
+  //In the viewport
+  root: null,
+  //How many percent of the element is visable in the viewport to fire off the event
+  threshold: 0,
+  rootMargin: "-80px"
+})
+obs.observe(sectionHeroEL)
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
